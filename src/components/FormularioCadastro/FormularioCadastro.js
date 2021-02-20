cf5343260299540da49c034e5cf0746a56f28773
lucaspires-source <60105171+lucaspires-source@users.noreply.github.com>
2021-02-20 12:08:39 -0300

    Aula 03.02

--- src/components/FormularioCadastro/FormularioCadastro.js
+++ src/components/FormularioCadastro/FormularioCadastro.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 import { TextField, Button, Switch, FormControlLabel } from "@material-ui/core";
 
 
 const  FormularioCadastro = () => {
+    const [nome,setNome] = useState();
+  
   return (
-    <form>
+    <form onSubmit={(e) =>{
+      e.preventDefault()
+      console.log(e.target.value)
+    }}>
       <TextField
+        onChange={(e) =>{
+
+          nome = e.target.value;
+          if(nome.length>1){
+            nome = nome.substr(0,3)
+          }
+        }}
         id="nome"
         label="Nome"
         variant="outlined"
@@ -38,7 +50,7 @@ const  FormularioCadastro = () => {
         control={<Switch name="promocoes" defaultChecked color="primary" />}
       />
 
-      <Button type="submit" variant="contained" color="primary">
+      <Button type="submit" variant="contained" color="primary" >
         Cadastrar
       </Button>
     </form>
